07a3209a0323ad1965397ef409e3a700df89bfe2
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-05-30 20:36:47 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const emojiDictionary = {
+  "🍕": "Pizza",
+  "😢": "Sad face",
+  "🥡": "Takeout",
+  "😑": "Expressionaless face",
+  "💕": "Love",
+  "👍": "Thumbs Up",
+  "👨‍💻": "A boy coding",
+  "⚽": "Football",
+  "🌍": "Earth",
+  "🤳": "Taking selfie",
+  "🔥": "Fire",
+  "💸": "Money with wings",
+  "😎": "Face with sunglasses",
+  "🎁": "Wrapped gift",
+  "🎢": "Roller Coaster",
+  "🧤": "Gloves",
+  "💋": "Kisss mark",
+  "🌭": "Hotdog",
+  "🛹": "Skateboard",
+  "🚂": "Locomotive",
+  "🚧": "Construction barrier",
+  "🍓": "Strawberry",
+  "🛸": "UFO",
+  "🏓": "Table tennis",
+  "🥋": "Martial arts Uniform",
+  "🎮": "Video game controller",
+  "🎵": "Musical note",
+  "🪔": "Diya lamp",
+  "👻": "Ghost",
+  "💩": "Pile of poo",
+  "🐱‍👤": "Ninja Cat",
+  "🦘": "Kangaroo",
+  "🧠": "Brain",
+  "🏇": "Horse racing",
+  "🧧": "Red envelopr",
+  "🧵": "Thread",
+  "🦺": "Safety vest",
+  "🥾": "Hiking shoe",
+  "🏀": "Basketball",
+  "🎸": "Guitar",
+  "💊": "Medicine pill",
+  "📮": "Postbox",
+  "🌴": "Palm tree"
+};
+
+let emojis = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [emojiMeaning, setEmojiMeaning] = useState(
+    "Enter above or click an Emoji below to know its meaning."
+  );
+
+  function emojiMeaningHandler(event) {
+    let userInput = event.target.value;
+    let meaning = emojiDictionary[userInput];
+
+    if (userInput in emojiDictionary) {
+      setEmojiMeaning(meaning);
+    } else {
+      meaning = "Not Found in our Database.";
+      setEmojiMeaning(meaning);
+    }
+
+    if (userInput === "") {
+      meaning = "Enter above or click an Emoji below to know its meaning.";
+      setEmojiMeaning(meaning);
+    }
+  }
+
+  function emojiClickHandler(e) {
+    setEmojiMeaning(emojiDictionary[e]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">Emoji Meaning</h1>
+      <input onChange={emojiMeaningHandler}></input>
+      <h2 className="emoji-meaning">{emojiMeaning}</h2>
+      <div className="emoji-list">
+        {emojis.map((emoji) => (
+          <li key={emoji} onClick={() => emojiClickHandler(emoji)}>
+            {emoji}
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
